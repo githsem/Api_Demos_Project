e3a9835d35f0a207fec36b005446c0ae9b1bcd7f
Hasan Hüseyin Semiz <hhsem49@gmail.com>
2023-11-16 01:52:17 +0100

    edited

--- src/main/java/com/example/utils/Locators.java
+++ src/main/java/com/example/utils/Locators.java
@@ -4,7 +4,5 @@ import org.openqa.selenium.By;
 
 public interface Locators {
 
-    By lMediaList = By.className("android.widget.TextView");
-
-    By lResultFinal = By.id("result_final");
+    By lInputBox = By.id("edit");
 }
