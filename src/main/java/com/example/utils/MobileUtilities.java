b492fb325f2844e6b899f3dd9188775f573317e8
Hasan Hüseyin Semiz <hhsem49@gmail.com>
2023-11-16 01:52:08 +0100

    edited

--- src/main/java/com/example/utils/MobileUtilities.java
+++ src/main/java/com/example/utils/MobileUtilities.java
@@ -12,73 +12,66 @@ import static com.example.utils.Driver.*;
 
 public class MobileUtilities {
 
-    public static AppiumDriver<MobileElement> openApp(Device device, App app){
+    public static AppiumDriver<MobileElement> openApp(Device device, App app) {
         runAppiumService();
         return getDriver(device, app);
     }
-    public static void closeApp(){
+
+    public static void closeApp() {
         getDriver().closeApp();
         stopAppiumService();
     }
 
-    public static void clickWithText(String text){
-        getDriver().findElement(By.xpath("//*[@text='"+text+"']")).click();
+    public static void clickWithText(String text) {
+        getDriver().findElement(By.xpath("//*[@text='" + text + "']")).click();
     }
 
-    public static By getLocatorByText(String text){
-        return By.xpath("//*[@text='"+text+"']");
+    public static By getLocatorByText(String text) {
+        return By.xpath("//*[@text='" + text + "']");
     }
 
-    public static WebElement getElementByText(String text){
-        return getDriver().findElement(By.xpath("//*[@text='"+text+"']"));
+    public static WebElement getElementByText(String text) {
+        return getDriver().findElement(By.xpath("//*[@text='" + text + "']"));
     }
 
-    public static void swipeV(double startPoint, double endPoint){
+    public static void swipeV(double startPoint, double endPoint) {
         int width = getDriver().manage().window().getSize().width;
         int height = getDriver().manage().window().getSize().height;
 
         new TouchAction<>(getDriver())
-                .press(PointOption.point(width/2, (int) (height * startPoint)))
-                .moveTo(PointOption.point(width/2, (int) (height * endPoint)))
+                .press(PointOption.point(width / 2, (int) (height * startPoint)))
+                .moveTo(PointOption.point(width / 2, (int) (height * endPoint)))
                 .release()
                 .perform();
     }
 
-    public static void swipeUntil(By locator, double startPoint, double endPoint){
+    public static void swipeUntil(By locator, double startPoint, double endPoint) {
 
-        while(getDriver().findElements(locator).size() == 0){
+        while (getDriver().findElements(locator).size() == 0) {
             swipeV(startPoint, endPoint);
         }
 
     }
 
-    public static void swipeAndClick(By locator, double startPoint, double endPoint){
-        while(getDriver().findElements(locator).size() == 0){
+    public static void swipeAndClick(By locator, double startPoint, double endPoint) {
+        while (getDriver().findElements(locator).size() == 0) {
             swipeV(startPoint, endPoint);
         }
         getDriver().findElement(locator).click();
     }
 
-    public static void clickToCoordinate(int x, int y){
+    public static void clickToCoordinate(int x, int y) {
         new TouchAction<>(getDriver())
                 .press(PointOption.point(x, y))
                 .release()
                 .perform();
     }
 
-    public static void activateBackgroundApp(String appPackage){
+    public static void activateBackgroundApp(String appPackage) {
         ApplicationState applicationState = getDriver().queryAppState(appPackage);
-        if(applicationState.equals(ApplicationState.RUNNING_IN_BACKGROUND)){
+        if (applicationState.equals(ApplicationState.RUNNING_IN_BACKGROUND)) {
             getDriver().activateApp(appPackage);
         }
     }
-
-    public static void clickToNumbers(Integer i){
-        getDriver().findElementByAccessibilityId(String.valueOf(i)).click();
-    }
-    public static void clickToOperators(String operator){
-        getDriver().findElementByAccessibilityId(operator).click();
-    }
-
 
 }
